03a737f2acb265a850894a74f3db52bb444df498
Oakekywe <oakekywephyohann99@gmail.com>
2023-02-09 19:35:06 +0630

    make signup login design component

---
+++ src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <h2>Login</h2>
+  <form @submit.prevent="login">
+    <input type="email" placeholder="Email" v-model="email">
+    <input type="password" placeholder="Password" v-model="password">
+    <button>Login</button>
+  </form>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+export default {
+    setup(){
+        const email= ref("")
+        const password= ref("")
+        const login= ()=>{
+            console.log(displayName.value, email.value, password.value);
+        }
+
+        return {email, password, login}
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Signup.vue
@@ -0,0 +1,29 @@
+<template>
+  <h2>Sign Up</h2>
+  <form @submit.prevent="signUp">
+    <input type="text" placeholder="Display name" v-model="displayName">
+    <input type="email" placeholder="Email" v-model="email">
+    <input type="password" placeholder="Password" v-model="password">
+    <button>Sign Up</button>
+  </form>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+export default {
+    setup(){
+        const displayName= ref("")
+        const email= ref("")
+        const password= ref("")
+        const signUp= ()=>{
+            console.log(displayName.value, email.value, password.value);
+        }
+
+        return {displayName, email, password, signUp}
+    }
+}
+</script>
+
+<style>
+
+</style>
