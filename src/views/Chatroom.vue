2b6b4f48a167ce538a0524086ca4e39b453b9cb7
Oakekywe <oakekywephyohann99@gmail.com>
2023-02-10 19:22:42 +0630

    after logout redirect to home with Watch function

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -5,10 +5,23 @@
 </template>
 
 <script>
+import getUser from '@/composables/getUser'
 import Navbar from '../components/Navbar'
+import { watch } from '@vue/runtime-core'
+import { useRouter } from 'vue-router'
+
 export default {
   components: { Navbar },
+  setup(){
+    let {user}= getUser()
+    let router= useRouter()
 
+    watch(user,()=>{
+      if(!user.value){
+        router.push('/')
+      }
+    })
+  }
 }
 </script>
 
